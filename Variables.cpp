6a5286b3a99806b5b1e83bab1c8fa0b0d0d5347f
Summaya Jawaid <summayaaa3@gmail.com>
2026-06-10 16:50:38 -0700

    Using variables in c++ (SUM program)
    📘 **Variables in C++**
    
    This file demonstrates basic use of variables in C++ by taking two numbers as input and calculating their sum.
    
    A simple step forward in my C++ journey .

---
+++ Variables.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter ths value of first number"<<endl;
+    cin>>a;
+    cout<<"Emter the value of second number"<<endl;
+    cin>>b;
+    cout <<"The sum of the numbers is "<<a<<"+"<<b<<endl;
+    cout<<a+b<<endl;
+    return 0;
+}
