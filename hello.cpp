44b42a3eaa9cb31af344db87fbe38c8e3b665c89
Summaya Jawaid <summayaaa3@gmail.com>
2026-06-10 16:29:16 -0700

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main()
 {
+        // learning the concept of cout and comments 
     cout<<"Hello World";
     return 0;
 }
-// learning git and github
+        // learning git and github
